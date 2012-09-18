bbb8397d9c03bab6e3c7b261fc2f212b60578006
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2012-09-18 18:37:28 +0900

    impl cidr coverter test app.

--- src/main/java/com/kaoriya/qb/ip_range/App.java
+++ src/main/java/com/kaoriya/qb/ip_range/App.java
@@ -1,5 +1,7 @@
 package com.kaoriya.qb.ip_range;
 
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,21 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        try {
+            DataReader r = new DataReader(System.in);
+            while (true) {
+                IPv4RangeData d = r.read();
+                if (d == null) {
+                    break;
+                }
+                List<CIDR> list = CIDRUtils.toCIDR(d);
+                System.out.println(d.toString());
+                for (CIDR v : list) {
+                    System.out.println("    " + v);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
