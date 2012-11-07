60d87a9ed3466a84970785bed3307534f62b204c
MURAOKA Taro <koron.kaoriya@gmmail.com>
2012-11-07 14:11:26 +0900

    implement double array bench

--- src/main/java/com/kaoriya/qb/ip_range/StrApp.java
+++ src/main/java/com/kaoriya/qb/ip_range/StrApp.java
@@ -12,6 +12,7 @@ import java.util.Random;
 
 import org.ardverk.collection.StringKeyAnalyzer;
 import org.ardverk.collection.PatriciaTrie;
+import org.trie4j.doublearray.DoubleArray;
 
 public class StrApp
 {
@@ -53,6 +54,7 @@ public class StrApp
         Random r = new Random(0);
         BufferedReader reader = newReader(file);
         try {
+            matcher.addStart();
             while (true) {
                 String line = reader.readLine();
                 if (line == null) {
@@ -62,6 +64,7 @@ public class StrApp
                 }
                 matcher.add(line);
             }
+            matcher.addEnd();
         } finally {
             reader.close();
         }
@@ -90,7 +93,9 @@ public class StrApp
             this.name = name;
         }
         public final String getName() { return this.name; }
+        public void addStart() throws Exception {}
         public abstract void add(String s) throws Exception;
+        public void addEnd() throws Exception {}
         public abstract boolean match(String s) throws Exception;
     }
 
@@ -102,14 +107,11 @@ public class StrApp
             int loop)
         throws Exception
     {
-        System.out.format("%1$s: loading #1 (ratio:%2$d loop:%3$d)\n",
+        System.out.format("%1$s: running (expected ratio:%2$d loop:%3$d)\n",
                 matcher.getName(), percentage, loop);
         loadKeys(hostFile, matcher, percentage);
-
-        System.out.format("%1$s: loading #2\n", matcher.getName());
         List<String> urlList = loadLines(urlFile);
 
-        System.out.format("%1$s: matching #2\n", matcher.getName());
         int countMatch = 0;
         int countUnmatch = 0;
         long start = System.nanoTime();
@@ -125,11 +127,11 @@ public class StrApp
         long end = System.nanoTime();
         long duration = end - start;
 
-        System.out.format("%4$s: match=%1$d unmatch=%2$d total=%3$d\n",
-                countMatch, countUnmatch, countMatch + countUnmatch,
-                matcher.getName());
+        System.out.format("%1$s: ratio:%2$.2f%%\n",
+                matcher.getName(),
+                countMatch * 100d / (countMatch + countUnmatch));
 
-        System.out.format("%4$s: duration: %1$d.%2$03ds (%3$d nano)\n",
+        System.out.format("%4$s: duration:%1$d.%2$03ds (%3$d nano)\n",
                 duration / 1000000000,
                 (duration % 1000000000) / 1000000,
                 duration,
@@ -138,7 +140,7 @@ public class StrApp
 
     public static class PatriciaTrieMatcher extends Matcher {
         private final PatriciaTrie<String, Boolean> trie;
-        PatriciaTrieMatcher() {
+        public PatriciaTrieMatcher() {
             super("PatriciaTrie");
             this.trie = new PatriciaTrie(StringKeyAnalyzer.INSTANCE);
         }
@@ -153,10 +155,36 @@ public class StrApp
         }
     }
 
+    public static class DoubleArrayMatcher extends Matcher {
+        private DoubleArray table = null;
+        private org.trie4j.patricia.simple.PatriciaTrie trie = null;
+        public DoubleArrayMatcher() {
+            super("DoubleArray");
+        }
+        @Override
+        public void addStart() throws Exception {
+            this.trie = new org.trie4j.patricia.simple.PatriciaTrie();
+        }
+        @Override
+        public void add(String s) throws Exception {
+            this.trie.insert(s);
+        }
+        @Override
+        public void addEnd() throws Exception {
+            this.table = new DoubleArray(trie);
+            this.trie = null;
+        }
+        @Override
+        public boolean match(String s) throws Exception {
+            return this.table.contains2(s);
+        }
+    }
+
     public static void main(File hostFile, File urlFile)
         throws Exception
     {
         run(hostFile, urlFile, new PatriciaTrieMatcher(), 100, 100);
+        run(hostFile, urlFile, new DoubleArrayMatcher(), 100, 100);
     }
 
     public static void main(String[] args) {
